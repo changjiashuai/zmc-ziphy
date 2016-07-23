d68a698ad84b12d53675440754e936156f4cb41b
Gerasimenko Mike <github@gerasimenko.me>
2016-07-24 00:41:52 +0200

    Added test host application

---
+++ ZiphyTestHost/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZiphyTestHost
+//
+//  Created by Mihail Gerasimenko on 7/24/16.
+//  Copyright © 2016 Zeta Project Germany. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZiphyTestHost/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZiphyTestHost
+//
+//  Created by Mihail Gerasimenko on 7/24/16.
+//  Copyright © 2016 Zeta Project Germany. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
